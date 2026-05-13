152251217ee5acbd84da992849c6f5a386ee8c38
Purwanto <438758+po3nx@users.noreply.github.com>
2026-05-13 14:57:31 +0800

    Enable TypeScript ignoreBuildErrors in config
    Added TypeScript configuration to ignore build errors.

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
+  typescript: {
+    // Dangerously allow production builds to successfully complete even if
+    // your project has TypeScript errors.
+    ignoreBuildErrors: true,
+  },
 }
 
 module.exports = nextConfig
